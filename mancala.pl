f939a2841977db0d6b8e2110b8b1c5fe24137d5a
maayankestler <maayan3002@gmail.com>
2020-10-02 13:54:39 +0300

    use CurrentPLayer as player number instead of player name

--- mancala.pl
+++ mancala.pl
@@ -5,122 +5,92 @@
 start_game(Player1, Player2, PitsNumber, PiecesInPit, Player1Func, Player2Func):-
     create_initialized_list(PitsNumber, PiecesInPit, Row),
     create_initialized_list(2, Row, Board),
-    % Board=[[0,0,0,0,3,0], [0,0,0,0,0,0]],
+    % Board=[[0,0,0,0,1,0], [0,0,0,0,0,0]],
     Player1Score is 0,
     Player2Score is 0,
     print_board(Board, Player1, Player2, Player1Score, Player2Score),
-    play(Board, Player1, Player2, Player1, Player1Score, Player2Score, Player1Func, Player2Func), !.
+    play(Board, Player1, Player2, 1, Player1Score, Player2Score, Player1Func, Player2Func), !.
 
 start_game:-
     start_game('Maayan', 'CompAI', 6, 4, user_input, random_ai), !.
 
 
-play(Board, Player1, Player2, CurrentPlayer, Player1Score, Player2Score, Player1Func, Player2Func):-
+play(Board, Player1, Player2, CurrentPlayerNumber, Player1Score, Player2Score, Player1Func, Player2Func):-
     (
-        CurrentPlayer = Player1,
-        RowIndex = 1,
-        nth1(RowIndex, Board, Row),
-        sumlist(Row, 0),
-        write(Player1), write(" can't move "), nl,
-        play(Board, Player1, Player2, Player2, Player1Score, Player2Score, Player1Func, Player2Func)
+        CurrentPlayerNumber = 1,
+        Player = Player1
     ;
-        CurrentPlayer = Player2,
-        RowIndex = 2,
-        nth1(RowIndex, Board, Row),
-        sumlist(Row, 0),
-        write(Player2), write(" can't move "), nl,
-        play(Board, Player1, Player2, Player1, Player1Score, Player2Score, Player1Func, Player2Func)
-    ), !.
+        CurrentPlayerNumber = 2,
+        Player = Player2
+    ),
+    nth1(CurrentPlayerNumber, Board, Row),
+    sumlist(Row, 0),
+    write(Player), write(" can't move "), nl,
+    NextPlayer is (CurrentPlayerNumber mod 2) + 1,
+    play(Board, Player1, Player2, NextPlayer, Player1Score, Player2Score, Player1Func, Player2Func), !.
 
-play(Board, Player1, Player2, CurrentPlayer, Player1Score, Player2Score, Player1Func, Player2Func):-
+play(Board, Player1, Player2, CurrentPlayerNumber, Player1Score, Player2Score, Player1Func, Player2Func):-
     (
-        CurrentPlayer = Player1,
+        CurrentPlayerNumber = 1,
+        Player = Player1,
         Func = Player1Func,
         Colour = 'blue'
     ;
-        CurrentPlayer = Player2,
+        CurrentPlayerNumber = 2,
+        Player = Player2,
         Func = Player2Func,
         Colour = 'green'
     ),
-    write("turn: "), ansi_format([bold,fg(Colour)], '~w', [CurrentPlayer]), nl,
-    call(Func, Board, RowIndex, Pit),
-    ansi_format([bold,fg(Colour)], '~w', [CurrentPlayer]), ansi_format([bold,fg(cyan)], ' move is: ~w', [Pit]), nl,
+    write("turn: "), ansi_format([bold,fg(Colour)], '~w', [Player]), nl,
+    call(Func, Board, CurrentPlayerNumber, Pit),
+    ansi_format([bold,fg(Colour)], '~w', [Player]), ansi_format([bold,fg(cyan)], ' move is: ~w', [Pit]), nl,
     (
-        do_move(Board, Player1, Player2, CurrentPlayer, Player1Score, Player2Score, Pit, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer),
+        do_move(Board, CurrentPlayerNumber, Player1Score, Player2Score, Pit, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer),
         print_board(NewBoard, Player1, Player2, NewPlayer1Score, NewPlayer2Score),
         next_move(NewBoard, Player1, Player2, NextPlayer, NewPlayer1Score, NewPlayer2Score, Player1Func, Player2Func)
     ;
         ansi_format([bold,fg(red)], 'unvalid move, try again', []), nl,
-        play(Board, Player1, Player2, CurrentPlayer, Player1Score, Player2Score, Player1Func, Player2Func)
+        play(Board, Player1, Player2, CurrentPlayerNumber, Player1Score, Player2Score, Player1Func, Player2Func)
     ), !.
 
-next_move(NewBoard, Player1, Player2, _, NewPlayer1Score, NewPlayer2Score, Player1Func, Player2Func):-
+next_move(NewBoard, Player1, Player2, _, NewPlayer1Score, NewPlayer2Score, _, _):-
     check_winner(NewBoard, Player1, Player2, NewPlayer1Score, NewPlayer2Score), !.
 
 next_move(NewBoard, Player1, Player2, NextPlayer, NewPlayer1Score, NewPlayer2Score, Player1Func, Player2Func):-
     play(NewBoard, Player1, Player2, NextPlayer, NewPlayer1Score, NewPlayer2Score, Player1Func, Player2Func), !.
 
-do_move(Board, Player1, Player2, CurrentPlayer, Player1Score, Player2Score, Pit, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer):-
+do_move(Board, CurrentPlayerNumber, Player1Score, Player2Score, Pit, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer):-
     integer(Pit),
     Pit > 0,
-    (
-        CurrentPlayer = Player1,
-        RowIndex = 1
-    ;
-        CurrentPlayer = Player2,
-        RowIndex = 2
-    ),
-    nth1(RowIndex, Board, Row),
+    nth1(CurrentPlayerNumber, Board, Row),
     replace_nth1(Row, Pit, PiecesCount, 0, NewRow),
     PiecesCount > 0,
-    replace_nth1(Board, RowIndex, _, NewRow, TempBoard),
+    replace_nth1(Board, CurrentPlayerNumber, _, NewRow, TempBoard),
     NewPitIndex is Pit + 1,
-    put_pieces(TempBoard, PiecesCount, RowIndex, NewPitIndex, Player1, Player2, CurrentPlayer, 
+    put_pieces(TempBoard, PiecesCount, CurrentPlayerNumber, NewPitIndex, CurrentPlayerNumber, 
             Player1Score, Player2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer), !.
 
-put_pieces(Board, 0, _, 1, _, _, CurrentPlayer, Player1Score, Player2Score, Board, Player1Score, Player2Score, CurrentPlayer):- !.
+put_pieces(Board, 0, _, 1, CurrentPlayerNumber, Player1Score, Player2Score, Board, Player1Score, Player2Score, CurrentPlayerNumber):- !.
 
-put_pieces(Board, 0, RowIndex, PitIndex, Player1, Player2, CurrentPlayer, Player1Score, Player2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer):-
+put_pieces(Board, 0, RowIndex, PitIndex, CurrentPlayerNumber, Player1Score, Player2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer):-
+    NextPlayer is (CurrentPlayerNumber mod 2) + 1,
     (
-        CurrentPlayer = Player1,
-        NextPlayer = Player2,
-        (
-            RowIndex =:= 1,
-            SecondeRowIndex is 2,
-            IsPlayerRow = true
-        ;
-            RowIndex =:= 2,
-            IsPlayerRow = false
-        )
-    ;
-        CurrentPlayer = Player2,
-        NextPlayer = Player1,
-        (
-            RowIndex =:= 2,
-            SecondeRowIndex is 1,
-            IsPlayerRow = true
-        ;
-            RowIndex =:= 1,
-            IsPlayerRow = false
-        )
-    ),
-    LastPitIndex is PitIndex - 1,
-    nth1(RowIndex, Board, Row),
-    (
-        IsPlayerRow,
+        CurrentPlayerNumber = RowIndex,
+        LastPitIndex is PitIndex - 1,
+        nth1(RowIndex, Board, Row),
         replace_nth1(Row, LastPitIndex, 1, 0, NewRow),
-        nth1(SecondeRowIndex, Board, SecondeRow),
+        nth1(NextPlayer, Board, SecondeRow),
         length(SecondeRow, SecondeRowLength),
         SecondePitIndex is SecondeRowLength - LastPitIndex + 1,
         replace_nth1(SecondeRow, SecondePitIndex, X, 0, NewSecondeRow),
         X > 0,
         (
-            CurrentPlayer = Player1,
+            CurrentPlayerNumber = 1,
             NewPlayer1Score is Player1Score + X + 1,
             NewPlayer2Score is Player2Score,
             NewBoard = [NewRow, NewSecondeRow]
         ;
-            CurrentPlayer = Player2,
+            CurrentPlayerNumber = 2,
             NewPlayer2Score is Player2Score + X + 1,
             NewPlayer1Score is Player1Score,
             NewBoard = [NewSecondeRow, NewRow]
@@ -131,7 +101,7 @@ put_pieces(Board, 0, RowIndex, PitIndex, Player1, Player2, CurrentPlayer, Player
         NewBoard = Board
     ), !.
 
-put_pieces(Board, PiecesCount, RowIndex, PitIndex, Player1, Player2, CurrentPlayer,
+put_pieces(Board, PiecesCount, RowIndex, PitIndex, CurrentPlayerNumber,
            Player1Score, Player2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer):-
     nth1(RowIndex, Board, Row),
     length(Row, RowLength),
@@ -142,46 +112,35 @@ put_pieces(Board, PiecesCount, RowIndex, PitIndex, Player1, Player2, CurrentPlay
     NewPitIndex is PitIndex + 1,
     replace_nth1(Row, PitIndex, X, NX, NewRow),
     replace_nth1(Board, RowIndex, Row, NewRow, TempBoard),
-    put_pieces(TempBoard, NewPiecesCount, RowIndex, NewPitIndex, Player1, Player2, CurrentPlayer,
+    put_pieces(TempBoard, NewPiecesCount, RowIndex, NewPitIndex, CurrentPlayerNumber,
                Player1Score, Player2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer), !.
     
-put_pieces(Board, PiecesCount, RowIndex, PitIndex, Player1, Player2, CurrentPlayer,
+put_pieces(Board, PiecesCount, RowIndex, PitIndex, CurrentPlayerNumber,
            Player1Score, Player2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer):-
     nth1(RowIndex, Board, Row),
     length(Row, RowLength),
     PitIndex =:= RowLength + 1,
     NewPitIndex = 1,
+    NewRowIndex is (RowIndex mod 2) + 1,
     (
-        CurrentPlayer = Player1,
+        CurrentPlayerNumber = RowIndex,
+        NewPiecesCount is PiecesCount - 1,
         (
-            RowIndex = 1,
+            CurrentPlayerNumber = 1,
             TempPlayer1Score is Player1Score + 1,
-            NewPiecesCount is PiecesCount - 1,
-            NewRowIndex = 2,
-            put_pieces(Board, NewPiecesCount, NewRowIndex, NewPitIndex, Player1, Player2, CurrentPlayer,
-                       TempPlayer1Score, Player2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer)
+            TempPlayer2Score is Player2Score
         ;
-            RowIndex = 2,
-            NewRowIndex = 1,
-            put_pieces(Board, PiecesCount, NewRowIndex, NewPitIndex, Player1, Player2, CurrentPlayer,
-                       Player1Score, Player2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer)
+            CurrentPlayerNumber = 2,
+            TempPlayer2Score is Player2Score + 1,
+            TempPlayer1Score is Player1Score
         )
     ;
-        CurrentPlayer = Player2,
-        (
-            RowIndex = 2,
-            TempPlayer2Score is Player2Score + 1,
-            NewPiecesCount is PiecesCount - 1,
-            NewRowIndex = 1,
-            put_pieces(Board, NewPiecesCount, NewRowIndex, NewPitIndex, Player1, Player2, CurrentPlayer,
-                       Player1Score, TempPlayer2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer)
-        ;
-            RowIndex = 1,
-            NewRowIndex = 2,
-            put_pieces(Board, PiecesCount, NewRowIndex, NewPitIndex, Player1, Player2, CurrentPlayer,
-                       Player1Score, Player2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer)
-        )
-    ), !.
+        TempPlayer2Score is Player2Score,
+        TempPlayer1Score is Player1Score,
+        NewPiecesCount is PiecesCount
+    ),
+    put_pieces(Board, NewPiecesCount, NewRowIndex, NewPitIndex, CurrentPlayerNumber,
+               TempPlayer1Score, TempPlayer2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer), !.
 
 check_winner(Board, Player1, Player2, Player1Score, Player2Score):-
     nth1(1, Board, Row1),
@@ -196,6 +155,7 @@ check_winner(Board, Player1, Player2, Player1Score, Player2Score):-
         Player2Score > Player2Score,
         write("The winner is "), write(Player2), write("!!!!"), nl
     ;
+        Player1Score = Player2Score,
         write("Tie"), nl
     ), !.
 
@@ -227,6 +187,6 @@ random_ai(Board, RowIndex, Pit):-
     length(Row, RowLength),
     random_between(1, RowLength, Pit).
 
-user_input(Board, RowIndex, Pit):-
+user_input(_, _, Pit):-
     write("enter pit number to play: "),
     read(Pit).
