968982f264814d7ca7f24de38b74d7522c3461a0
maayankestler <maayan3002@gmail.com>
2020-10-13 23:43:59 +0300

    add comments + better result_posistions

--- mancala.pl
+++ mancala.pl
@@ -1,19 +1,34 @@
+% Programmer: Maayan Kestler
+% Name File: mancal.pl
+% Description: console mancla game with alphabeta ai player
+% Synopsys: to satrt playing mancala run the start_game predicate
+
 :- use_module(library(lists)).
 :- use_module(library(clpfd)).
 :- use_module(library(random)).
 
+% the main predicate of the program, start a mancala game
+% Player1- the name of the first player
+% Player2- the name of the second  player
+% PitsNumber- the number of pits in each row of the board
+% PiecesInPit- the number of pieces in each pit at the start of the game
+% Player1Func- the playing logic of the first player
+% Player2Func- the playing logic of the second player
 start_game(Player1, Player2, PitsNumber, PiecesInPit, Player1Func, Player2Func):-
     create_initialized_list(PitsNumber, PiecesInPit, Row),
     create_initialized_list(2, Row, Board),
-    % Board=[[0,0,0,0,0,0], [0,0,0,0,0,1]],
     Player1Score is 0,
     Player2Score is 0,
     print_board(Board, Player1, Player2, Player1Score, Player2Score),
     play(Board, Player1, Player2, 1, Player1Score, Player2Score, Player1Func, Player2Func), !.
 
+% start the game with deafult arguments
 start_game:-
     start_game('Maayan', 'CompAI', 6, 4, user_input, alphabeta_ai), !.
 
+% play a move in the game
+% Board- the mancala board made of list containing two lists (one for each row)
+% CurrentPlayerNumber- the number of the current player (1 for Player1 and 2 for Player2)
 play(Board, Player1, Player2, CurrentPlayerNumber, Player1Score, Player2Score, Player1Func, Player2Func):-
     (
         CurrentPlayerNumber = 1,
@@ -34,16 +49,20 @@ play(Board, Player1, Player2, CurrentPlayerNumber, Player1Score, Player2Score, P
         print_board(NewBoard, Player1, Player2, NewPlayer1Score, NewPlayer2Score),
         next_move(NewBoard, Player1, Player2, NextPlayer, NewPlayer1Score, NewPlayer2Score, Player1Func, Player2Func)
     ;
-        ansi_format([bold,fg(red)], 'unvalid move, try again', []), nl,
-        play(Board, Player1, Player2, CurrentPlayerNumber, Player1Score, Player2Score, Player1Func, Player2Func)
+        ansi_format([bold,fg(red)], 'invalid move, try again', []), nl,
+        play(Board, Player1, Player2, CurrentPlayerNumber, Player1Score, Player2Score, Player1Func, Player2Func) % play the turn again in case of invalid move
     ), !.
 
 next_move(NewBoard, Player1, Player2, _, NewPlayer1Score, NewPlayer2Score, _, _):-
-    check_winner(NewBoard, Player1, Player2, NewPlayer1Score, NewPlayer2Score), !.
+    check_winner(NewBoard, Player1, Player2, NewPlayer1Score, NewPlayer2Score), !. % someone won, the game end
 
 next_move(NewBoard, Player1, Player2, NextPlayer, NewPlayer1Score, NewPlayer2Score, Player1Func, Player2Func):-
-    play(NewBoard, Player1, Player2, NextPlayer, NewPlayer1Score, NewPlayer2Score, Player1Func, Player2Func), !.
+    play(NewBoard, Player1, Player2, NextPlayer, NewPlayer1Score, NewPlayer2Score, Player1Func, Player2Func), !. % play the next move
 
+% do a move in the game
+% PitIndex- the of the pit the player chose to play (starting from 1)
+% NewBoard- the board adter playing the move
+% NextPlayer- the player that need to play the next move (can be the same player again)
 do_move(Board, CurrentPlayerNumber, Player1Score, Player2Score, PitIndex, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer):-
     integer(PitIndex),
     nth1(CurrentPlayerNumber, Board, Row),
@@ -54,13 +73,22 @@ do_move(Board, CurrentPlayerNumber, Player1Score, Player2Score, PitIndex, NewBoa
     put_pieces(TempBoard, PiecesCount, CurrentPlayerNumber, NewPitIndex, CurrentPlayerNumber, 
             Player1Score, Player2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer), !.
 
+% check if a given pit index is a valid move (the index in range and there are pieces in the pit)
+% Row- the row of the player that want to do the move
 valid_move(Row, PitIndex):-
     length(Row, RowLength),
     between(1, RowLength, PitIndex),
-    nth1(PitIndex, Row, Elem),
-    Elem > 0.
+    nth1(PitIndex, Row, Pieces),
+    Pieces > 0.
 
-put_pieces(Board, 0, _, 1, CurrentPlayerNumber, Player1Score, Player2Score, Board, Player1Score, Player2Score, NextPlayer):-
+% put_pieces(Board, PiecesCount, RowIndex, PitIndex, CurrentPlayerNumber, Player1Score, Player2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer):
+% move the pieces from the played pit to the next pits in the board
+% PiecesCount- amount of pieces left to put
+% RowIndex- the index of the row the pieces need to be put in
+% PitIndex- th index of the pit in the row the pieces need to be put in
+% 
+% put last piece in result pit, the player get another turn
+put_pieces(Board, 0, _, 1, CurrentPlayerNumber, Player1Score, Player2Score, Board, Player1Score, Player2Score, NextPlayer):- 
     nth1(CurrentPlayerNumber, Board, Row),
     (
         sum_list(Row, 0),
@@ -69,6 +97,7 @@ put_pieces(Board, 0, _, 1, CurrentPlayerNumber, Player1Score, Player2Score, Boar
         NextPlayer = CurrentPlayerNumber
     ), !.
 
+% put last piece, if in empty pit get other player piece from facing pit
 put_pieces(Board, 0, RowIndex, PitIndex, CurrentPlayerNumber, Player1Score, Player2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer):-
     OtherPlayer is (CurrentPlayerNumber mod 2) + 1,
     (
@@ -105,6 +134,7 @@ put_pieces(Board, 0, RowIndex, PitIndex, CurrentPlayerNumber, Player1Score, Play
         NextPlayer = OtherPlayer
     ), !.
 
+% put pieces in same row
 put_pieces(Board, PiecesCount, RowIndex, PitIndex, CurrentPlayerNumber,
            Player1Score, Player2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer):-
     nth1(RowIndex, Board, Row),
@@ -119,6 +149,7 @@ put_pieces(Board, PiecesCount, RowIndex, PitIndex, CurrentPlayerNumber,
     put_pieces(TempBoard, NewPiecesCount, RowIndex, NewPitIndex, CurrentPlayerNumber,
                Player1Score, Player2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer), !.
     
+% move to the next row
 put_pieces(Board, PiecesCount, RowIndex, PitIndex, CurrentPlayerNumber,
            Player1Score, Player2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer):-
     nth1(RowIndex, Board, Row),
@@ -146,11 +177,13 @@ put_pieces(Board, PiecesCount, RowIndex, PitIndex, CurrentPlayerNumber,
     put_pieces(Board, NewPiecesCount, NewRowIndex, NewPitIndex, CurrentPlayerNumber,
                TempPlayer1Score, TempPlayer2Score, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer), !.
 
+% check if there is a winner
 check_winner(Board, Player1, Player2, Player1Score, Player2Score):-
     sum_board(Board, Sum),
     Sum =:= 0,
     declare_winner(Player1, Player2, Player1Score, Player2Score), !.
 
+% declare who the winner is
 declare_winner(Player1, _, Player1Score, Player2Score):-
     Player1Score > Player2Score, !,
     write("The winner is "), write(Player1), write("!!!!"), nl.
@@ -170,13 +203,14 @@ sum_board(Board, Sum):-
     sum_list(Row2, Sum2),
     Sum is Sum1 + Sum2.
 
-% % https://www.swi-prolog.org/pldoc/man?predicate=nth0/4
+% https://www.swi-prolog.org/pldoc/doc_for?object=nth0/4
 replace_nth1(List, Index, OldElem, NewElem, NewList) :-
     % predicate works forward: Index,List -> OldElem, Transfer
     nth1(Index,List,OldElem,Transfer),
     % predicate works backwards: Index,NewElem,Transfer -> NewList
     nth1(Index,NewList,NewElem,Transfer), !.
 
+% print the mancala board
 print_board(Board, Player1, Player2, Player1Score, Player2Score):-
     ansi_format([bold,fg(blue)], '~w', [Player1]),  write(" score is: "), write(Player1Score), nl,
     ansi_format([bold,fg(green)], '~w', [Player2]),  write(" score is: "), write(Player2Score), nl,
@@ -190,28 +224,37 @@ create_initialized_list(Length, Value, List):-
     length(List,Length), 
     maplist(=(Value), List), !.
 
+% playin logic, play random valid pit
 random_ai(Board, CurrentPlayerNumber, PitIndex):-
     nth1(CurrentPlayerNumber, Board, Row),
     findall(Pit, valid_move(Row, Pit), ValidMoves),
     random_member(PitIndex, ValidMoves).
 
+% playing logic, play by user input
 user_input(_, _, PitIndex):-
     write("enter Pit number to play: "),
     read(PitIndex).
 
+% playing logic, play by alphabeta algorithm with depth of 9
 alphabeta_ai(Board, CurrentPlayerNumber, PitIndex):-
     alphabeta_ai(Board, CurrentPlayerNumber, PitIndex, 9), !.
 
+% playing logic, play by alphabeta algorithm
 alphabeta_ai(Board, CurrentPlayerNumber, PitIndex, Depth):-
-    set_prolog_flag(stack_limit, 6_147_483_648),
+    set_prolog_flag(stack_limit, 6_442_450_944), % define stack size
     sum_board(Board, Beta),
     Alpha is -Beta,
     alphabeta(mancala_pos(Board, CurrentPlayerNumber, 0, 0, -1), Alpha, Beta, mancala_pos(_, _, _, _, PitIndex), _, Depth), !.
 
-%  The alpha-beta algorithm
+%  The alpha-beta algorithm (based on the book)
+%  use static val when Depth is 0
 alphabeta(Pos, _, _, Pos, Val, 0):-
     staticval(Pos, Val), !.
 
+% general case alpha-beta algorithm
+% Pos describe with mancala_pos(Board, CurrentPlayerNumber, Player1Score, Player2Score, PitIndex)
+% the score use for the heuristic function
+% PitIndex is the index of the pit played in the last turn (-1 if it's the first runing)
 alphabeta(Pos, Alpha, Beta, GoodPos, Val, Depth):-
     moves(Pos, Poslist), !,
     (
@@ -227,6 +270,7 @@ alphabeta(Pos, Alpha, Beta, GoodPos, Val, Depth):-
         staticval(Pos, Val)
     ).
 
+% MaxPlayer- true if the parent node is a max player
 boundedbest([Pos | Poslist], Alpha, Beta, GoodPos, GoodVal, Depth, MaxPlayer):-
     NewDepth is Depth - 1,
     alphabeta(Pos, Alpha, Beta, _, Val, NewDepth),
@@ -261,21 +305,26 @@ betterof(Pos, Val, _, Val1, Pos, Val, MaxPlayer) :- % Pos better than Pos1
 
 betterof(_, _, Pos1, Val1, Pos1, Val1, _). % Otherwise Pos1 better
 
+% heuristic function, calculate the value of given mancala_pos
 staticval(mancala_pos(_, _, Player1Score, Player2Score, _), Val):-
     Val is Player1Score - Player2Score, !.
 
+% player1 is the max player
 max_to_move(mancala_pos(_, CurrentPlayerNumber, _, _, _)):-
     CurrentPlayerNumber = 1, !.
 
+% player2 is the min player
 min_to_move(mancala_pos(_, CurrentPlayerNumber, _, _, _)):-
     CurrentPlayerNumber = 2, !.
 
-moves(mancala_pos(Board, CurrentPlayerNumber, Player1Score, Player2Score, _), Poslist):-
+% Poslist contain all the possible positions that can be reached by playing a move from the given mancala_pos
+moves(Pos, Poslist):-
+    findall(ResultPos, result_posistions(Pos, ResultPos), Poslist).
+
+% true if ResultPos is is a mancala_pos that can be reached from the given mancala_pos
+result_posistions(mancala_pos(Board, CurrentPlayerNumber, Player1Score, Player2Score, _), ResultPos):-
     nth1(CurrentPlayerNumber, Board, Row),
     findall(PitIndex, valid_move(Row, PitIndex), ValidMoves),
-    findall(ResultPos, result_posistions(mancala_pos(Board, CurrentPlayerNumber, Player1Score, Player2Score, _), ValidMoves, ResultPos), Poslist).
-
-result_posistions(mancala_pos(Board, CurrentPlayerNumber, Player1Score, Player2Score, _), ValidMoves, ResultPos):-
     member(PitIndex, ValidMoves),
     do_move(Board, CurrentPlayerNumber, Player1Score, Player2Score, PitIndex, NewBoard, NewPlayer1Score, NewPlayer2Score, NextPlayer),
     ResultPos = mancala_pos(NewBoard, NextPlayer, NewPlayer1Score, NewPlayer2Score, PitIndex).
